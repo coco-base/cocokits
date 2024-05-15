d5d238b3a570650860ca34d5219f0f3f726048f1
Milad <miladfm@gmail.com>
2024-05-15 18:54:38 +0200

    refactor(theme-core): replace utility functions with common-utils library

--- packages/themes/core/src/lib/theme-changed.ts
+++ packages/themes/core/src/lib/theme-changed.ts
@@ -1,6 +1,8 @@
-import { BehaviorSubject, filter, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+
+import { skipNullish } from '@coco-kits/common-utils';
 
 import { ThemeChangedEvent } from './common.model';
 
 export const themeChangedSubject$ = new BehaviorSubject<ThemeChangedEvent | null>(null);
-export const themeChanged$ = themeChangedSubject$.pipe(filter((value) => !!value)) as Observable<ThemeChangedEvent>;
+export const themeChanged$ = themeChangedSubject$.pipe(skipNullish());
