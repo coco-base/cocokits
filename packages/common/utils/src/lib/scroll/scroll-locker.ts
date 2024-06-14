c4fe2e9762e0f8cb63168dce61a0d30c6fa2f6eb
Milad <miladfm@gmail.com>
2024-06-14 13:42:19 +0200

    feat(common-utils): add scroll-locker utils

---
+++ packages/common/utils/src/lib/scroll/scroll-locker.ts
@@ -0,0 +1,84 @@
+type LockChangeCallback = (isLock: boolean) => void;
+
+export class ScrollLocker {
+  private static instance: ScrollLocker;
+
+  public static globalInstance() {
+    if (!ScrollLocker.instance) {
+      ScrollLocker.instance = new ScrollLocker();
+    }
+
+    return ScrollLocker.instance;
+  }
+
+  private callbacks = new Set<LockChangeCallback>();
+
+  public get isLocked() {
+    return document.documentElement.style.position === 'fixed';
+  }
+
+  public lock(hideScrollbar = false) {
+    if (this.isLocked) {
+      return;
+    }
+
+    const body = document.body;
+    const documentElement = document.documentElement;
+
+    const hasBodyScrolled = body.scrollHeight > window.innerHeight || body.scrollWidth > window.innerWidth;
+
+    if (hasBodyScrolled) {
+      const documentRect = documentElement.getBoundingClientRect();
+
+      const pageTop = 0;
+      const pageLeft = 0;
+      const top =
+        -documentRect.top || document.body.scrollTop || window.scrollY || documentElement.scrollTop || pageTop;
+      const left =
+        -documentRect.left || document.body.scrollLeft || window.scrollX || documentElement.scrollLeft || pageLeft;
+
+      documentElement.style.top = -top + 'px';
+      documentElement.style.left = -left + 'px';
+
+      documentElement.style.position = 'fixed';
+      documentElement.style.width = '100%';
+
+      hideScrollbar ? (documentElement.style.overflow = 'hidden') : (documentElement.style.overflowY = 'scroll');
+
+      this.callbacks.forEach((callback) => callback(true));
+    }
+  }
+
+  public unlock() {
+    if (!this.isLocked) {
+      return;
+    }
+
+    const documentElement = document.documentElement;
+    documentElement.style.overflow = '';
+    documentElement.style.overflowY = '';
+    documentElement.style.position = '';
+    documentElement.style.width = '';
+
+    const pageTop = Math.abs(parseFloat(documentElement.style.top));
+    const pageLeft = Math.abs(parseFloat(documentElement.style.left));
+    documentElement.scrollTo({
+      top: isNaN(pageTop) ? 0 : pageTop,
+      left: isNaN(pageLeft) ? 0 : pageLeft,
+    });
+
+    documentElement.style.removeProperty('top');
+    documentElement.style.removeProperty('left');
+    documentElement.style.removeProperty('overflow');
+
+    this.callbacks.forEach((callback) => callback(false));
+  }
+
+  public addLockChangeListener(callback: (isLock: boolean) => void) {
+    this.callbacks.add(callback);
+  }
+
+  public removeLockChangeListener(callback: (isLock: boolean) => void) {
+    this.callbacks.delete(callback);
+  }
+}
