330877977057e7b811cd96931490c2e918175a0b
Milad <miladfm@gmail.com>
2024-07-10 22:02:47 +0200

    feat(angular-checkbox): create checkbox empty library

--- packages/themes/core/src/lib/class-names/css-selector-render.ts
+++ packages/themes/core/src/lib/class-names/css-selector-render.ts
@@ -1,7 +1,5 @@
 import { isNullish } from '@cocokits/common-utils';
 
-import { UIComponentsPropName } from '../model/ui-component.model';
-
 export function cssSelectorRender(
   block: string,
   element: string | number | boolean,
@@ -9,9 +7,3 @@ export function cssSelectorRender(
 ) {
   return isNullish(modifier) ? `${block}__${element}` : `${block}__${element}--${modifier}`;
 }
-
-export const cssSelectorVariantElement: Record<UIComponentsPropName, string> = {
-  type: 'Host Element',
-  size: 'Svg Element',
-  color: 'Svg Element',
-};
