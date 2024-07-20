dcb25420a68d3362fd5bf326b2b830cd63a9eb67
Milad <miladfm@gmail.com>
2024-07-20 15:02:01 +0200

    feat(angular-radio): create ts concept of radio button and group

--- packages/angular/radio/src/lib/radio/radio.component.ts
+++ packages/angular/radio/src/lib/radio/radio.component.ts
@@ -1,22 +1,63 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  contentChildren,
+  effect,
+  ElementRef,
+  forwardRef,
+  inject,
+  input,
+  model,
+  output,
+  signal,
+  viewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { fromAttr } from '@cocokits/common-angular-utils';
 import { getRadioButtonClassNames, getRadioGroupClassNames, ThemeUIComponentPropValue } from '@cocokits/core';
 import { UIComponentConfig } from '@cocokits/core/angular';
+
+/**
+ * To avoid get cycle dependencies, we have to create radio-group and radio-button in a single file.
+ */
+
+// Increasing integer for generating unique ids for checkbox components.
+let nextUniqueId = 1;
+
+export interface RadioChange<T> {
+  /** The radio button that emits the change event. */
+  source: RadioButtonComponent<T>;
+
+  /** The value of the radio button. */
+  value: T;
+}
+
+/**
+ * Provider Expression that allows cck-radio-group to register as a ControlValueAccessor. This
+ * allows it to support [(ngModel)] and ngControl.
+ */
+export const RADIO_GROUP_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => RadioGroupComponent),
+  multi: true,
+};
 
 @Component({
   standalone: true,
   imports: [],
   selector: 'cck-radio-group',
   template: '<ng-content></ng-content>',
-  styleUrls: ['./radio-group.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
+  providers: [RADIO_GROUP_CONTROL_VALUE_ACCESSOR],
   host: {
     '[class]': 'classNames().host',
   },
 })
-export class RadioGroupComponent {
+export class RadioGroupComponent<T = unknown> implements ControlValueAccessor {
   private uiComponentConfig = inject(UIComponentConfig);
 
   /**
@@ -63,6 +104,33 @@ export class RadioGroupComponent {
    */
   public additional = fromAttr<Record<string, ThemeUIComponentPropValue>>({ prefix: 'data-cck-' });
 
+  /** Name of the radio button group. All radio buttons inside this group will use this name. */
+  public name = input<string>(`cck-radio-group-${nextUniqueId++}`);
+
+  /** The currently selected radio button */
+  public selected = input<T>();
+
+  /** Whether the radio group is disabled */
+  public disabled = input<boolean>();
+
+  /**
+   * Event emitted when the group value changes.
+   * Change events are only emitted when the value changes due to user interaction with
+   * a radio button (the same behavior as `<input type-"radio">`).
+   */
+  public readonly change = output<RadioChange<T>>();
+
+  private selectedRadio = signal<RadioButtonComponent<T> | null>(null);
+
+  private __onSelectedRadioChanged = effect(() => {
+    const selectedRadio = this.selectedRadio();
+    if (selectedRadio) {
+      this.change.emit({ source: selectedRadio, value: selectedRadio.value() });
+    }
+  });
+
+  private radios = contentChildren<RadioButtonComponent>(forwardRef(() => RadioButtonComponent));
+
   protected classNames = computed(() =>
     getRadioGroupClassNames(
       {
@@ -74,6 +142,57 @@ export class RadioGroupComponent {
       this.uiComponentConfig
     )
   );
+
+  public _setSelectedRadio(radio: RadioButtonComponent<T>) {
+    this.selectedRadio.set(radio);
+  }
+
+  /**
+   * onTouch function registered via registerOnTouch (ControlValueAccessor).
+   * @internal
+   */
+  onTouched: () => any = () => {
+    // Do nothing
+  };
+
+  /** The method to be called in order to update ngModel */
+  _controlValueAccessorChangeFn: (value: any) => void = () => {
+    // Do nothing
+  };
+
+  /**
+   * Sets the model value. Implemented as part of ControlValueAccessor.
+   * @param value
+   */
+  writeValue(_value: any) {
+    // Do nothing
+  }
+
+  /**
+   * Registers a callback to be triggered when the model value changes.
+   * Implemented as part of ControlValueAccessor.
+   * @param fn Callback to be registered.
+   */
+  registerOnChange(fn: (value: any) => void) {
+    this._controlValueAccessorChangeFn = fn;
+  }
+
+  /**
+   * Registers a callback to be triggered when the control is touched.
+   * Implemented as part of ControlValueAccessor.
+   * @param fn Callback to be registered.
+   */
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Sets the disabled state of the control. Implemented as a part of ControlValueAccessor.
+   * @param isDisabled Whether the control should be disabled.
+   */
+  setDisabledState(_isDisabled: boolean) {
+    // Do nothing
+  }
 }
 
 @Component({
@@ -85,10 +204,10 @@ export class RadioGroupComponent {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   host: {
-    '[class]': 'classNames().host',
+    '[class]': 'hostClassNames()',
   },
 })
-export class RadioButtonComponent {
+export class RadioButtonComponent<T = unknown> {
   private uiComponentConfig = inject(UIComponentConfig);
 
   /**
@@ -99,6 +218,8 @@ export class RadioButtonComponent {
    */
   public type = input<string>();
 
+  private effectedType = computed(() => this.type() ?? this.radioGroup?.type());
+
   /**
    * The size of radio-button.
    *
@@ -107,6 +228,8 @@ export class RadioButtonComponent {
    */
   public size = input<string>();
 
+  private effectedSize = computed(() => this.size() ?? this.radioGroup?.size());
+
   /**
    * The color of radio-button.
    *
@@ -114,6 +237,8 @@ export class RadioButtonComponent {
    * This allows for more flexible styling options if the desired size is not available in the selected theme.
    */
   public color = input<string>();
+
+  private effectedColor = computed(() => this.color() ?? this.radioGroup?.color());
 
   /**
    * A signal for attributes starting with `data-cck`.
@@ -135,15 +260,82 @@ export class RadioButtonComponent {
    */
   public additional = fromAttr<Record<string, ThemeUIComponentPropValue>>({ prefix: 'data-cck-' });
 
+  private effectedAdditional = computed(() => ({ ...this.additional(), ...this.radioGroup?.additional() }));
+
+  private radioGroup = inject(RadioGroupComponent<T>, { optional: true });
+
+  /** Analog to HTML 'name' attribute used to group radios for unique selection. */
+  public name = input<string>();
+
+  protected effectedName = computed(() => this.name() || this.radioGroup?.name());
+
+  /** Whether this radio button is checked. */
+  public checked = model<boolean>();
+
+  protected effectedChecked = computed(() => this.checked() || this.radioGroup?.selected() === this.value());
+
+  /** The value of this radio button. */
+  public value = input.required<T>();
+
+  /** Whether the radio button is disabled. */
+  public disabled = input<boolean>();
+
+  protected effectedDisabled = computed(() => this.disabled() || this.radioGroup?.disabled());
+
+  /** The unique ID for the radio button. If none is supplied, it will be auto-generated. */
+  public id = input(`cck-radio-${nextUniqueId++}`);
+
+  /**
+   * Event emitted when the group value changes.
+   * Change events are only emitted when the value changes due to user interaction with
+   * a radio button (the same behavior as `<input type-"radio">`).
+   */
+  readonly change = output<RadioChange<T>>();
+
+  /** The native `<input type="radio">` element */
+  private inputElement = viewChild<ElementRef<HTMLInputElement>>('input');
+
   protected classNames = computed(() =>
     getRadioButtonClassNames(
       {
-        type: this.type(),
-        size: this.size(),
-        color: this.color(),
-        additional: this.additional(),
+        type: this.effectedType(),
+        size: this.effectedSize(),
+        color: this.effectedColor(),
+        additional: this.effectedAdditional(),
       },
       this.uiComponentConfig
     )
   );
+
+  protected hostClassNames = computed(() => {
+    const classNames = [...this.classNames().host];
+
+    if (this.checked()) {
+      classNames.push(...this.classNames().checked);
+    } else {
+      classNames.push(...this.classNames().unchecked);
+    }
+
+    if (this.disabled()) {
+      classNames.push(...this.classNames().disabled);
+    }
+
+    return classNames;
+  });
+
+  /** Focuses the radio button. */
+  public focus(options?: FocusOptions): void {
+    this.inputElement()?.nativeElement.focus(options);
+  }
+
+  /** Triggered when the radio button receives an interaction from the user. */
+  protected _onInputChange(event: Event) {
+    // We always have to stop propagation on the change event.
+    // Otherwise the change event, from the input element, will bubble up and
+    // emit its event object to the `change` output.
+    event.stopPropagation();
+
+    this.change.emit({ source: this, value: this.value() });
+    this.radioGroup?._setSelectedRadio(this);
+  }
 }
--- packages/angular/radio/stories/radio-button/dev.stories.ts
+++ packages/angular/radio/stories/radio-button/dev.stories.ts
@@ -1,18 +1,20 @@
+import { action } from '@storybook/addon-actions';
 import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
 
 import { AngularStoriesMeta, AngularStoryObj } from '@cocokits/core';
 import { UIComponentConfig } from '@cocokits/core/angular';
 import { getSelectedCckTheme } from '@cocokits/storybook-theme-switcher';
 
-import { RadioButtonComponent } from '../../src/lib/radio/radio.component';
+import { RadioButtonComponent, RadioGroupComponent } from '../../src/lib/radio/radio.component';
 
 const meta: AngularStoriesMeta = {
-  component: RadioButtonComponent,
+  component: RadioGroupComponent,
   title: 'Dev/RadioButton',
   tags: ['!autodocs'],
   decorators: [
     componentWrapperDecorator((story) => `<div class="flex gap-24">${story}</div>`),
     moduleMetadata({
+      imports: [RadioButtonComponent],
       providers: [
         {
           provide: UIComponentConfig,
@@ -22,16 +24,152 @@ const meta: AngularStoriesMeta = {
     }),
   ],
   argTypes: {
-    type: { control: 'text' },
-    color: { control: 'text' },
-    size: { control: 'text' },
+    groupType: { control: 'text' },
+    groupColor: { control: 'text' },
+    groupSize: { control: 'text' },
+    groupName: { control: 'text' },
+    groupSelected: { control: 'text' },
+    groupDisabled: { control: 'boolean' },
+
+    radio1Type: { control: 'text' },
+    radio1Color: { control: 'text' },
+    radio1Size: { control: 'text' },
+    radio1Name: { control: 'text' },
+    radio1Checked: { control: 'boolean' },
+    radio1Value: { control: 'text' },
+    radio1Disabled: { control: 'boolean' },
+    radio1Id: { control: 'text' },
+
+    radio2Type: { control: 'text' },
+    radio2Color: { control: 'text' },
+    radio2Size: { control: 'text' },
+    radio2Name: { control: 'text' },
+    radio2Checked: { control: 'boolean' },
+    radio2Value: { control: 'text' },
+    radio2Disabled: { control: 'boolean' },
+    radio2Id: { control: 'text' },
   },
 };
 
 export default meta;
 
-export const Default: AngularStoryObj<RadioButtonComponent> = {
+export const Default: AngularStoryObj<any> = {
+  args: {},
+  argTypes: {
+    groupName: { table: { disable: true } },
+    radio1Name: { table: { disable: true } },
+    radio2Name: { table: { disable: true } },
+    radio1Value: { table: { disable: true } },
+    radio2Value: { table: { disable: true } },
+    radio1Id: { table: { disable: true } },
+    radio2Id: { table: { disable: true } },
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+      radio1Value: 'Radio1Value',
+      radio2Value: 'Radio2Value',
+      onGroupChange: action('On Group Selected Change'),
+      onChange: action('On Selected Change'),
+    },
+    template: `
+      <cck-radio-group
+        [type]="groupType"
+        [color]="groupColor"
+        [size]="groupSize"
+        [selected]="groupSelected"
+        [disabled]="groupDisabled"
+        (change)="onGroupChange($event)">
+
+          <cck-radio-button
+            [type]="radio1Type"
+            [color]="radio1Color"
+            [size]="radio1Size"
+            [checked]="radio1Checked"
+            [value]="radio1Value"
+            [disabled]="radio1Disabled"
+            (change)="onChange($event)">
+            Radio Button 1
+          </cck-radio-button>
+
+          <cck-radio-button
+            [type]="radio2Type"
+            [color]="radio2Color"
+            [size]="radio2Size"
+            [checked]="radio2Checked"
+            [value]="radio2Value"
+            [disabled]="radio2Disabled"
+            (change)="onChange($event)">
+            Radio Button 2
+          </cck-radio-button>
+
+      </cck-radio-group>
+    `,
+  }),
+};
+
+export const NameAndID: AngularStoryObj<RadioButtonComponent> = {
   args: {
     // color: 'brand',
   },
+  parameters: {},
+  render: (args) => ({
+    props: {
+      ...args,
+      radio1Value: 'Radio1Value',
+      radio2Value: 'Radio2Value',
+    },
+    template: `
+      <cck-radio-group
+        [type]="groupType"
+        [color]="groupColor"
+        [size]="groupSize"
+        [name]="groupName"
+        [selected]="groupSelected"
+        [disabled]="groupDisabled">
+
+          <cck-radio-button
+            [type]="radio1Type"
+            [color]="radio1Color"
+            [size]="radio1Size"
+            [name]="radio1Name"
+            [checked]="radio1Checked"
+            [value]="radio1Value"
+            [disabled]="radio1Disabled"
+            [id]="radio1Id">
+            Radio Button 1
+          </cck-radio-button>
+
+          <cck-radio-button
+            [type]="radio2Type"
+            [color]="radio2Color"
+            [size]="radio2Size"
+            [name]="radio2Name"
+            [checked]="radio2Checked"
+            [value]="radio2Value"
+            [disabled]="radio2Disabled"
+            [id]="radio1Id">
+            Radio Button 2
+          </cck-radio-button>
+
+      </cck-radio-group>
+    `,
+  }),
+};
+
+export const NgModel: AngularStoryObj<RadioButtonComponent> = {
+  args: {
+    // color: 'brand',
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+    },
+    template: `
+      <cck-radio-group>
+        <cck-radio-button>Radio Button 1</cck-radio-button>
+        <cck-radio-button>Radio Button 2</cck-radio-button>
+      </cck-radio-group>
+    `,
+  }),
 };
