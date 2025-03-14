ae617ddd8fca97c310c8b3763b592ac9d981e85a
Milad Faghihi <miladfm@gmail.com>
2025-03-14 18:58:57 +0100

    chore: add README file to the dist folder for react libraries

--- packages/react/radio/vite.config.ts
+++ packages/react/radio/vite.config.ts
@@ -2,6 +2,7 @@
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
+import copy from 'rollup-plugin-copy';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -20,6 +21,10 @@ export default defineConfig({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
       aliasesExclude: [/^@cocokits\//],
+    }),
+    copy({
+      targets: [{ src: 'README.md', dest: '../../../dist/packages/react/radio' }],
+      hook: 'writeBundle',
     }),
   ],
 
